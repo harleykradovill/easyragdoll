ecea84349a3093b878ffbe2d936a05b98badb397
Harley Kradovill <43226649+harleykradovill@users.noreply.github.com>
2024-01-13 14:52:21 -0600

    add toggle

--- client.lua
+++ client.lua
@@ -2,13 +2,17 @@
 -- https://github.com/harleykradovill
 
 print("EasyRagdoll Loaded")
+local ragdollEnabled = false
 
 Citizen.CreateThread(function ()
 	while true do
 		Citizen.Wait(0)
 		if IsControlPressed(1, 47) then
-			SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, true, true, false)
-			print("Toggle Ragdoll")
+		    ragdollEnabled = not ragdollEnabled
+		    if ragdollEnabled then
+                SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, true, true, false)
+                print("Toggle Ragdoll")
+            end
 		end
 	end
 end)
