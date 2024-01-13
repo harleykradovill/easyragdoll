0b3b0b3c0c143f802b291992a5c2df0444c0070c
Harley Kradovill <43226649+harleykradovill@users.noreply.github.com>
2024-01-13 14:00:49 -0600

    set up keybind

--- client.cs
+++ client.cs
@@ -8,4 +8,19 @@ using CitizenFX.Core.UI;
 public class EasyRagdoll : BaseScript {
     private bool ragdolled = false;
     private Control ragdollKey = Control.MultiplayerInfo;
+
+    public Ragdoll() {
+    Tick += OnTick;
+    RegisterCommand("toggleragdoll", new Action(ToggleRagdoll), false);
+
+    if (API.RegisterKeyMapping("toggleRagdoll", "Toggle Ragdoll", "keyboard", "Z")) {
+        Debug.WriteLine("Keybind registered");
+    } else {
+        Debug.WriteLine("Keybind failed to register");
+    }
+
+
+
+    }
+
 }
