eedc442f66d3153d3fdbb9e52c5168e539515fa8
Harley Kradovill <43226649+harleykradovill@users.noreply.github.com>
2024-01-13 14:31:42 -0600

    add debug writelines

--- client.cs
+++ client.cs
@@ -5,6 +5,7 @@ using CitizenFX.Core;
 using CitizenFX.Core.Native;
 
 public class EasyRagdoll : BaseScript {
+    Debug.WriteLine("EasyRagdoll Load")
     public EasyRagdoll() {
         Tick += OnTick;
         EventHandlers["ragdoll:toggle"] += new Action(ToggleRagdoll);
@@ -13,10 +14,12 @@ public class EasyRagdoll : BaseScript {
     private void OnTick() {
         if (Game.IsControlPressed(1, Control.Jump)) {
             TriggerServerEvent("ragdoll:toggle");
+            Debug.WriteLine("Ragdoll event trig from client via tick")
         }
     }
 
     private void ToggleRagdoll() {
         API.SetPedToRagdoll(Game.PlayerPedId, 1000, 1000, 0, true, true, false);
+        Debug.WriteLine("Ragdoll event trig from client via toggleRagdoll")
     }
 }
