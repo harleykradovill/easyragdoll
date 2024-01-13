d2e1d00910a6ae437558c8cd59a6ea56aad13dd2
Harley Kradovill <43226649+harleykradovill@users.noreply.github.com>
2024-01-13 13:27:33 -0600

    add dependencies

---
+++ client.cs
@@ -0,0 +1,10 @@
+// Created by Harley Kradovill
+// https://github.com/harleykradovill
+
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using CitizenFX.Core.UI;
+
+public class EasyRagdoll : BaseScript {
+
+}
